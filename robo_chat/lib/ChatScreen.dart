4aad0f4f1468242fbf751d80fa908c881835bd10
showstopper728 <bestintheworld728@yahoo.com>
2019-10-15 20:07:18 +0200

    Fourth commit

---
+++ robo_chat/lib/ChatScreen.dart
@@ -0,0 +1,177 @@
+import 'dart:ffi';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:robo_chat/Message.dart';
+import 'LogIn.dart';
+import 'CircualrImage.dart';
+import 'SearchUser.dart';
+import 'main.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class ChatScreen extends StatefulWidget {
+  static const String id = "CHAT";
+  final User user;
+
+  const ChatScreen({Key key, this.user}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ChatScreenState(user);
+  }
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  User user;
+  final Reference = FirebaseDatabase.instance.reference();
+  String message = '';
+  String curEmail = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  List<Widget> widgets = [];
+
+  _ChatScreenState(this.user);
+
+  TextEditingController _c;
+
+  @override
+  void initState() {
+    getUser();
+    _c = new TextEditingController();
+    DatabaseReference dp = Reference.child('Messages');
+    dp
+      ..orderByChild('created_at').once().then((DataSnapshot snapshot) {
+        Map<dynamic, dynamic> values = snapshot.value;
+        values.forEach((key, value) {
+          setState(() {
+            String from = value['From'];
+            String to = value['To'];
+            String msg = value['Message'];
+            String eUser = user.email;
+            if (from == curEmail && eUser == to) {
+              widgets.add(Message(
+                from: from,
+                text: msg,
+                me: true,
+              ));
+              print(msg);
+            }
+            if (from == eUser && curEmail == to) {
+              widgets.add(Message(
+                from: from,
+                text: msg,
+                me: false,
+              ));
+            }
+          });
+        });
+      });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text(user.email),
+          leading: Icon(Icons.chat),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: widgets.length,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, i) {
+                    return widgets[i];
+                  }),
+              height: 380,
+            ),
+            Expanded(
+              child: Align(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        onChanged: (text) {
+                          message = text;
+                        },
+                        controller: _c,
+                        decoration: InputDecoration(
+                          hintText: 'Send Message',
+                          border: const OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            Reference.reference()
+                                .child('Messages')
+                                .push()
+                                .set(<String, String>{
+                              'From': curEmail,
+                              'To': user.email,
+                              "Message": message
+                            });
+                          });
+                          update();
+                          _c.text = ' ';
+                        },
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.send),
+                      ),
+                      margin: EdgeInsets.only(left: 5),
+                    )
+                  ],
+                ),
+                alignment: FractionalOffset.bottomCenter,
+              ),
+
+            )
+          ],
+        ));
+  }
+
+  getUser() async {
+    FirebaseUser user = await _auth.currentUser();
+    curEmail = user.email;
+  }
+
+  update() {
+    widgets = [];
+    DatabaseReference dp = Reference.child('Messages');
+    dp.once().then((DataSnapshot snapshot) {
+      Map<dynamic, dynamic> values = snapshot.value;
+      values.forEach((key, value) {
+        setState(() {
+          String from = value['From'];
+          String to = value['To'];
+          String msg = value['Message'];
+          String eUser = user.email;
+          if (from == curEmail && eUser == to) {
+            widgets.add(Message(
+              from: from,
+              text: msg,
+              me: true,
+            ));
+
+            print(msg);
+          }
+          if (from == eUser && curEmail == to) {
+            widgets.add(Message(
+              from: from,
+              text: msg,
+              me: false,
+            ));
+          }
+        });
+      });
+    });
+  }
+}
--- robo_chat/lib/Search.dart
+++ robo_chat/lib/Search.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:robo_chat/ChatScreen.dart';
 import 'LogIn.dart';
 import 'CircualrImage.dart';
 import 'SearchUser.dart';
@@ -24,45 +25,53 @@ class _SearchState extends State<Search> {
     // TODO: implement build
     return Scaffold(
         body: Column(
-      children: <Widget>[
-        Container(
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: "Search for a User by Email",
-              prefixIcon: Icon(Icons.search),
-              border: const OutlineInputBorder(),
-            ),
-            onChanged: (text) {
-              users = [];
-              DatabaseReference dp = Reference.child('Users');
-              dp.once().then((DataSnapshot snapshot) {
-                Map<dynamic, dynamic> values = snapshot.value;
-                values.forEach((key, value) {
-                  setState(() {
-                    String email = value['Email'];
-                    if(email.contains(text))
-                    users.add(User(email: email, id: key.toString()));
+          children: <Widget>[
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search for a User by Email",
+                  prefixIcon: Icon(Icons.search),
+                  border: const OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  users = [];
+                  DatabaseReference dp = Reference.child('Users');
+                  dp.once().then((DataSnapshot snapshot) {
+                    Map<dynamic, dynamic> values = snapshot.value;
+                    values.forEach((key, value) {
+                      setState(() {
+                        String email = value['Email'];
+                        if (email.contains(text))
+                          users.add(User(email: email, id: key.toString()));
+                      });
+                    });
                   });
-                });
-              });
-            },
-          ),
-          margin: EdgeInsets.only(top: 10),
-        ),
-        Expanded(
-
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: users.length,
-              itemBuilder: (context, i) {
-                return ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text(users[i].email),
-                  onTap: () {},
-                );
-              }),
-        )
-      ],
-    ));
+                },
+              ),
+              margin: EdgeInsets.only(top: 10),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: users.length,
+                  itemBuilder: (context, i) {
+                    return ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text(users[i].email),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ChatScreen(
+                                   user: users[i],
+                                ),
+                          ),);
+                      },
+                    );
+                  }),
+            )
+          ],
+        ));
   }
 }
